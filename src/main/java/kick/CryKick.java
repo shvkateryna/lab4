ed3dcfe7f98d39e2a3290997f4255b68fcbe4dc5
shvkateryna <116515521+shvkateryna@users.noreply.github.com>
2023-10-06 00:24:27 +0300

    Create CryKick.java

---
+++ src/main/java/kick/CryKick.java
@@ -0,0 +1,9 @@
+package kick;
+import lotr.Character;
+public class CryKick implements KickStrategy{
+    @Override
+    public void kick(Character whoKick, Character whoIsKicked) {
+        System.out.println(whoKick.getClass().getSimpleName() + ": 😭😭😭");
+        
+    }
+}
